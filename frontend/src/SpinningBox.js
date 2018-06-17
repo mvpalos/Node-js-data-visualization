261c47bdb467d7eb47fe1da29bc05fbdf4562a96
mvpalos <mariepe49@hotmail.com>
2018-06-17 15:33:40 -0400

    orbitControls working

--- frontend/src/SpinningBox.js
+++ frontend/src/SpinningBox.js
@@ -1,16 +1,19 @@
 import React, {Component} from 'react';
 import * as THREE from 'three';
+import OrbitControls from 'three-orbitcontrols';
 
 class SpinningBox extends Component{
     scene = new THREE.Scene();
 
 componentDidMount(){
+
+        /*CREATING VIEW */
     const {width, height } = this.props
     const scene = new THREE.Scene(),
           camera = new THREE.PerspectiveCamera(75, width/height, 0.1, 1000),
           renderer = new THREE.WebGLRenderer();
 
-        //this section resizes the canvas
+        /*BROWSER RESIZER */
           renderer.setSize(width , height)
 
           this.refs.anchor.appendChild(renderer.domElement);
@@ -19,34 +22,33 @@ componentDidMount(){
 			let height = window.innerHeight;
 			renderer.setSize(width, height);
 			camera.aspect = width/height;
-		    // let controls = new OrbitControls(camera, renderer);
+		    let controls = new OrbitControls(camera, renderer.domElement);
         });
-
-
-        
-        window.addEventListener("resize", function() {
+           window.addEventListener("resize", function() {
 			let width = window.innerWidth;
 			let height = window.innerHeight;
 			renderer.setSize(width, height);
 			camera.aspect = width/height;
 		});
-    
-          const geometry = new THREE.BoxGeometry(2, 2, 2),
+        /*SHAPE DETAILS */
+          const geometry = new THREE.SphereGeometry(2, 32, 10),
                 material = new THREE.MeshBasicMaterial({color: 0x2DFDB9, wireframe: true}),
-                cube = new THREE.Mesh(geometry, material);
+                sphere = new THREE.Mesh(geometry, material);
+            let light = new THREE.DirectionalLight(0Xffffff);
+                light.position.set(0,1,1).normalize();
 
-        scene.add(cube);
+        scene.add(sphere, light);
 
         camera.position.z = 5;
 
         function gameLoop(){
             requestAnimationFrame(gameLoop);
-                cube.rotation.x += 0.01;
-                cube.rotation.y += 0.01;
+            sphere.rotation.x += 0.01;
+            sphere.rotation.y += 0.01;
 
                 renderer.render(scene, camera);
             
-        }
+}
 
         gameLoop();
 
