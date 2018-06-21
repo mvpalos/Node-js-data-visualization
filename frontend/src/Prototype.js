c7b50062a8d1364e9ac928fe30c9bbe62e18464b
mvpalos <mariepe49@hotmail.com>
2018-06-21 14:21:28 -0400

    video setup including size and meta data components

--- frontend/src/Prototype.js
+++ frontend/src/Prototype.js
@@ -1,14 +1,84 @@
 import React, {Component} from 'react';
-import * as tf from '@tensorflow/tfjs';
+import * as posenet from '@tensorflow-models/posenet';
+import dat from 'dat.gui';
 
 class Prototype extends Component{
     constructor(){
         super()
     }
+/*checking for device according to OS and device type*/
+componentDidMount(){
+    const width = 600;
+    const height = 500;
+    // const stats = new Stats();
+
+function isAndroid(){
+    return /Android/i.test(navigator.userAgent);
+}
+
+function isiOS() {
+    return /iPhone|iPad|iPod/i.test(navigator.userAgent);
+  }
+
+  function isMobile(){
+      return isAndroid() || isiOS();
+  }
+
+  /*Loads a the camera to be used in demo*/
+    async function setUpCamera() {
+        if(!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia){
+            throw new Error(
+                'Browser API navigator.mediaDevices is not avalible');
+        }
+
+        //creating the video size
+        const video = document.getElementById('video');
+        video.width = videoWidth;
+        video.height = videoHeight;      
+
+        //awaiting promise for navigation detection
+        const mobile = isMobile();
+        const stream = await navigator.mediaDevices.getUserMedia({
+            'audio' : false,
+            'video' : {
+                facingMode : 'user',
+                width : mobile ? undefined : videoWidth,
+                height : mobile ? undefined : videoHeight
+            },
+        });
+        video.srcObject = stream;
+
+        return new Promise((resolve)=>{
+            //meta data refering to data about data ei. the loading time
+            video.onloadedmetadata = () => {
+                resolve(video);
+            };
+        });
+    }
+
+    /*creating a PLAY VIDEO BUTTON*/
+    async function loadVideo(){
+        //calling previouse setupCamera() involving 
+        //1.creating video size, 
+        //2. detectign device 
+        const video = await setUpCamera();
+        video.play();
+
+        return video;
+    }
+
+    const 
+}
+
+
     render(){
         return(
-            <div>
+            <div class>
+            <center>
+                <div className = "text">
                 <p>this is the prototype</p>
+            </div>
+            </center>
             </div>
         )
     }
--- frontend/src/Tensor.js
+++ frontend/src/Tensor.js
@@ -8,7 +8,16 @@ class Tensor extends Component{
     render(){
         return(
             <div>
-                <p>what is a tensor?</p>
+                <center>
+                <div className = "text">
+                <p>What is a Tensor?</p>
+                <p>In mathematics, tensors are geometric objects
+                   that describe linear relations between geometric 
+                   vectors, scalars, and other tensors. Elementary 
+                   examples of such relations include the dot product, 
+                   the cross product, and linear maps.</p>
+                </div> 
+                </center>  
                 <SpinningTensor width = {1300} height = {600} />
             </div>
         )
