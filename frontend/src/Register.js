852331141948eafca4a17934b673227b2b5503f9
mvpalos <mariepe49@hotmail.com>
2018-07-29 23:43:37 -0400

    part 2 registration page

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -11,4 +11,51 @@ class Register extends Component {
     this.registerHandler = this.registerHandler.bind(this);
     this.removeErrorHandler = this.removeErrorHandler.bind(this);
     }
+componentWillMount(){
+        axios.post('/validtoken',({jwt:localStorage.getItem("jwt")}))
+        .then((result)=>{
+            if(!result.data.error){
+                this.props.history.push("/feed");
+            }
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+   }
+   registerHandler(e){
+    e.preventDefault();
+    if (e.target.username && e.target.password)
+    {
+        axios.post("/register", {
+            username: e.target.username.value,
+            password: e.target.password.value
+        })
+        .then((results) =>
+        {
+            if (!results.data.error)
+            {
+                if (results.data.jwt)
+                {
+                    localStorage.setItem("jwt", results.data.jwt);
+                    this.props.history.push("/feed");
+                }
+            }
+            else
+            {
+                this.setState({
+                    error: results.data.reason
+                });
+            }
+        })
+        .catch((error) =>
+        {
+            console.log(error);
+        });
+    }
+}
+removeErrorHandler(){
+    this.setState({
+        error: []
+    })
+}
 }
